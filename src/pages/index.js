f6543b14a5a0db347383ecf4dfcc5da029b153b8
top-dev-mania <dev.mania.639@gmail.com>
2020-11-26 07:19:11 -0800

    feat [add] add glasswall function 1st

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,41 +5,50 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+// import Ticker from 'react-ticker'
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    // title: 'deep-File Inspection',
+    // imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    imageUrl: 'img/partnership-image-200x112.png',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        {/* deep-File Inspection breaks down the 
+         entire file and validates it against the 
+                format’s specification. */}
+        Glasswall Partners With Swedish Security Specialist link22 on Secure File Transfer
       </>
     ),
+    url: 'https://glasswallsolutions.com/glasswall-partners-with-link22/',
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    //title: 'Focus on What Matters',
+    imageUrl: 'img/glasswallapinewsfeed-200x112.png',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        {/* Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory. */}
+        Glasswall Launches its Cloud-hosted File Regeneration Service
       </>
     ),
+    url: 'https://glasswallsolutions.com/rebuild-api-launch/',
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    //title: 'Powered by React',
+    imageUrl: 'img/thumbnail1-200x112.jpg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        {/* Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer. */}
+        Glasswall Offers Free Cyber Protection to Small Businesses as COVID-19 Crisis Continues
       </>
     ),
+    url: 'https://glasswallsolutions.com/free-cyber-protection-for-small-businesses/',
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({imageUrl, title, description, url}) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
     <div className={clsx('col col--4', styles.feature)}>
@@ -50,6 +59,16 @@ function Feature({imageUrl, title, description}) {
       )}
       <h3>{title}</h3>
       <p>{description}</p>
+      <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={url}>
+              Read More
+            </Link>
+      </div>
     </div>
   );
 }
@@ -59,12 +78,23 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+
+    title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      {/* <Ticker mode="smoth" height = "100">
+        {({ index }) => (
+            <>
+                <h4>PRODUCT LAUNCH: Rebuild API, a cloud-hosted service that allows organisations to safely and securely import files and documents. FIND OUT MORE.</h4>
+            </>
+        )}
+      </Ticker> */}
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+          <h1 className="hero__title">Trust Every File.</h1>
+          {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+          <p className="hero__subtitle">We are a file regeneration and analytics company, and a leader 
+          in the field of CDR: Content Disarm and Reconstruction</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
@@ -75,6 +105,7 @@ function Home() {
               Get Started
             </Link>
           </div>
+          
         </div>
       </header>
       <main>
@@ -89,6 +120,7 @@ function Home() {
             </div>
           </section>
         )}
+      
       </main>
     </Layout>
   );
